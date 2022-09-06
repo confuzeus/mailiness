16a8235a6eaba745ec89d3b507d012d42f52be44
Josh Michael Karamuth <michael@confuzeus.com>
2022-09-06 13:32:17 +0400

    Add public exponent setting

--- src/mailiness/settings.py
+++ src/mailiness/settings.py
@@ -1,1 +1,5 @@
+# According to https://www.daemonology.net/blog/2009-06-11-cryptographic-right-answers.html
+RSA_PUBLIC_EXPONENT = 65537
+
+# Use 2048 for maximum compatibility as at year 2022.
 DKIM_KEY_SIZE = 2048
