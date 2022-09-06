19964b23c5cfaccd82122163643accac046b2c3e
Josh Michael Karamuth <michael@confuzeus.com>
2022-09-06 13:40:01 +0400

    Remove public key attribute

--- src/mailiness/dkim.py
+++ src/mailiness/dkim.py
@@ -5,7 +5,6 @@ from . import settings
 class DKIM:
     def __init__(self, selector: str, domain: str):
         self.private_key = None
-        self.public_key = None
         self.selector = selector
         self.domain = domain
 
