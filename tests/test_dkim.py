2ab45614bd0be46e6fc56ecf7278d612aea038f9
Josh Michael Karamuth <michael@confuzeus.com>
2022-09-06 15:50:53 +0400

    Stop manual key generation

--- tests/test_dkim.py
+++ tests/test_dkim.py
@@ -12,23 +12,18 @@ class DKIMTest(TestCase):
         self.dkim = DKIM(self.selector, self.domain)
 
     def test_new_dkim_instance_has_correct_attributes_set(self):
-        self.assertIsNone(self.dkim.private_key)
-        self.assertIsNone(self.dkim.public_key)
         self.assertEqual(self.dkim.selector, self.selector)
         self.assertEqual(self.dkim.domain, self.domain)
 
     def test_can_generate_private_key(self):
-        self.dkim.generate_key()
         self.assertIsInstance(self.dkim.private_key, RSAPrivateKey)
 
     def test_can_serialize_private_key_as_pem(self):
-        self.dkim.generate_key()
         pem = self.dkim.private_key_as_pem()
         self.assertIsInstance(pem, str)
         self.assertIn("BEGIN PRIVATE KEY", pem)
 
     def test_private_key_is_serialized_correctly(self):
-        self.dkim.generate_key()
         pem = self.dkim.private_key_as_pem()
         loaded = serialization.load_pem_private_key(pem.encode("utf-8"), password=None)
         self.assertIsInstance(loaded, RSAPrivateKey)
