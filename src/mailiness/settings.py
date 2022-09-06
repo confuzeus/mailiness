91aa3139375bdacef8ae76f4fcc637263719c6f7
Josh Michael Karamuth <michael@confuzeus.com>
2022-09-06 20:58:19 +0400

    Add dkim settings

--- src/mailiness/settings.py
+++ src/mailiness/settings.py
@@ -1,3 +1,7 @@
+DKIM_PRIVATE_KEY_DIRECTORY= "/var/lib/rspamd/dkim"
+
+DKIM_MAPS_PATH = "/etc/rspamd/dkim_selectors.map"
+
 # According to https://www.daemonology.net/blog/2009-06-11-cryptographic-right-answers.html
 RSA_PUBLIC_EXPONENT = 65537
 
