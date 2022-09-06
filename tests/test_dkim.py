1eb2cadd9fa9c1e8f361c31e53d4551c536791f6
Josh Michael Karamuth <michael@confuzeus.com>
2022-09-06 15:03:58 +0400

    Test that private key is serialized properly

--- tests/test_dkim.py
+++ tests/test_dkim.py
@@ -1,10 +1,11 @@
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 from unittest import TestCase
 
 from mailiness.dkim import DKIM
 
-class DKIMTest(TestCase):
 
+class DKIMTest(TestCase):
     def setUp(self):
         self.selector = "20220906"
         self.domain = "example.com"
@@ -25,3 +26,20 @@ class DKIMTest(TestCase):
         pem = self.dkim.private_key_as_pem()
         self.assertIsInstance(pem, str)
         self.assertIn("BEGIN PRIVATE KEY", pem)
+
+    def test_private_key_is_serialized_correctly(self):
+        self.dkim.generate_key()
+        pem = self.dkim.private_key_as_pem()
+        loaded = serialization.load_pem_private_key(pem.encode("utf-8"), password=None)
+        self.assertIsInstance(loaded, RSAPrivateKey)
+
+        loaded_private_numbers = loaded.private_numbers()
+        loaded_p = loaded_private_numbers.p
+        loaded_q = loaded_private_numbers.q
+
+        instance_private_numbers = self.dkim.private_key.private_numbers()
+        instance_p = instance_private_numbers.p
+        instance_q = instance_private_numbers.q
+
+        self.assertEqual(loaded_p, instance_p)
+        self.assertEqual(loaded_q, instance_q)
