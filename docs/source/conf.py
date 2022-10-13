26047a4d2a9302dcf38816cb1c0f980b3629f594
Josh Michael Karamuth <michael@confuzeus.com>
2022-10-13 12:35:27 +0400

    Bump version 0.2.0 -> 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Mailiness'
 copyright = '2022, Josh Michael Karamuth'
 author = 'Josh Michael Karamuth'
-release = '0.2.0'
+release = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
